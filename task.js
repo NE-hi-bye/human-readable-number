67f074c18e1cbf8ba39a65107c4f3dd95d9cc591
NE-hi-bye <hi-bye@bk.ru>
2022-06-23 21:01:37 +0400

    init: start task

--- task.js
+++ task.js
@@ -1,4 +1,4 @@
-module.exports = function toReadable(numberr) {
+function toReadable(numberr) {
     const arr = {
         0: "zero",
         1: "one",
@@ -31,7 +31,10 @@ module.exports = function toReadable(numberr) {
         90: "ninety",
         100: "hundred",
     };
+
     const number = String(numberr).split("");
+    console.log(numberr, number.length, number, number.slice(-2).join(""));
+    let res;
     const num3 = number.slice(-1).join("");
     const num2 = number.slice(-2, -1).join("");
     const num2teen = num2 + "0";
@@ -40,17 +43,31 @@ module.exports = function toReadable(numberr) {
     const numTo2 = number.slice(-2).join("");
 
     if (number.length === 3) {
+        console.log("Ook");
         if (numTo2 == 00) {
+            console.log(arr[num1], arr[100]);
             return arr[num1] + " " + arr[100];
         } else if (numTo2 <= 20) {
             if (numTo2 > 9) {
+                console.log(arr[num1], arr[100], arr[numTo2]);
                 return arr[num1] + " " + arr[100] + " " + arr[numTo2];
             } else {
-                return arr[num1] + " " + arr[100] + " " + arr[num3];
+                console.log(arr[num1], arr[100], arr[num3]);
+                res = arr[num1] + " " + arr[100] + " " + arr[num3];
+                return res;
             }
         } else if (num3 == 0) {
             return arr[num1] + " " + arr[100] + " " + arr[num2teen];
         } else {
+            console.log(arr[num1], arr[100], arr[num2teen], arr[num3]);
+            res =
+                arr[num1] +
+                " " +
+                arr[100] +
+                " " +
+                arr[num2teen] +
+                " " +
+                arr[num3];
             return (
                 arr[num1] +
                 " " +
@@ -75,4 +92,6 @@ module.exports = function toReadable(numberr) {
             return arr[num1teen];
         }
     }
-};
+}
+
+console.log(toReadable(852));
